8c25941bb311b0156c6d97c70a524d9482b255e9
rafpoke <lin.rafael0402@gmail.com>
2025-08-15 11:46:07 -0300

    solution update

--- medical_data_visualizer.py
+++ medical_data_visualizer.py
@@ -4,30 +4,47 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # 1
-df = None
+df = pd.read_csv("medical_examination.csv")
 
 # 2
-df['overweight'] = None
+BMI = (df['weight']/(df['height']**2) * 10000) > 25
+df['overweight'] = BMI.astype(int)
 
 # 3
+df['cholesterol'] = (df['cholesterol'] > 1).astype(int)
+df['gluc'] = (df['gluc'] > 1).astype(int)
+df['active'] = (df['active'] == 0).astype(int)
+
 
 
 # 4
 def draw_cat_plot():
     # 5
-    df_cat = None
+    df_cat = pd.melt(df, 
+                    id_vars=['cardio'],
+                    value_vars = ['cholesterol', 'gluc', 'smoke', 'alco', 'active', 'overweight'],
+                    var_name='variable',
+                    value_name='value')
 
 
     # 6
-    df_cat = None
+    df_cat = df_cat.groupby(['cardio', 'variable', 'value']).size().reset_index(name='total')
     
 
     # 7
+    cat_plot = sns.catplot(
+                        data=df_cat,
+                        x='variable',
+                        y='total',
+                        hue='value',
+                        col='cardio',
+                        kind='bar'
+                    )
 
 
 
     # 8
-    fig = None
+    fig = cat_plot.fig
 
 
     # 9
@@ -37,22 +54,39 @@ def draw_cat_plot():
 
 # 10
 def draw_heat_map():
+
+    df_heat = df
+
     # 11
-    df_heat = None
+    df_heat =df_heat[
+                        (df_heat['ap_lo'] <= df_heat['ap_hi']) &
+                        (df_heat['height'] >= df_heat['height'].quantile(0.025)) &
+                        (df_heat['height'] <= df_heat['height'].quantile(0.975)) &
+                        (df_heat['weight'] >= df_heat['weight'].quantile(0.025)) &
+                        (df_heat['weight'] <= df_heat['weight'].quantile(0.975))
+                    ]
 
     # 12
-    corr = None
+    corr = df_heat.corr()
 
     # 13
-    mask = None
+    # create a triangular matix to mask the correlation matrix for the heat map
+    mask = np.triu(np.ones(corr.shape, dtype=bool))
 
 
 
     # 14
-    fig, ax = None
+    fig, ax = plt.subplots(figsize = (12,10))
 
     # 15
 
+    sns.heatmap(corr, mask = mask, annot=True,       # show correlation values
+    fmt=".1f",        # format to 1 decimal place
+    cmap='coolwarm',  # color map
+    center=0,         # center colormap at 0
+    square=True,      # square cells
+    linewidths=0.5,   # lines between cells
+    cbar_kws={"shrink": 0.5})
 
 
     # 16
